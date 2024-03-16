2971280e40e1d3a10e396b909e91c6e6eba6ce58
TevinOgutu <tevinodhis@gmail.com>
2024-03-16 11:57:47 +0300

    Darting around

---
+++ README.md
@@ -0,0 +1,5 @@
+PLP Dart Hackathon.
+Dart Mastery on set. 
+Program 1 Display Personal Information.
+Program 2 Perform Mathematical Operations with Functions.
+Program 3 Determine Grade Based on Marks.
---
+++ program_1.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = "Tevin";
+  String school = "PLP School of cooding";
+  int age = 24;
+  String hobby = "Hiking";
+
+  print(
+      "My name is $name aged $age Years old and I'm from $school. My hobby is $hobby");
+}
