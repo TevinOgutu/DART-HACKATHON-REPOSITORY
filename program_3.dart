b2aa743079594476185b843df7cc61745f61e9af
TevinOgutu <tevinodhis@gmail.com>
2024-03-16 12:42:14 +0300

    Let's get nerdy

---
+++ program_3.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter your marks: ");
+  double marks = double.parse(stdin.readLineSync()!);
+
+  String grade = determineGrade(marks);
+  print(grade);
+}
+
+String determineGrade(double marks) {
+  String grade;
+
+  switch (marks ~/ 10) {
+    case 10:
+    case 9:
+    case 8:
+      grade = 'Excellent';
+      break;
+    case 7:
+      grade = 'Very Good';
+      break;
+    case 6:
+      grade = 'Good';
+      break;
+    default:
+      grade = 'Average';
+  }
+  return grade;
+}
